f95186795372dcd48050b213b5073554c7a1f0fd
developerfab <fab7696550@gmail.com>
2020-11-11 09:31:49 -0500

    Add campaign controller
    It includes the campaigns controller to list, create and show a
    campagin. It also includes test, routes and expceptions.

---
+++ app/controllers/campaigns_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class CampaignsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def index
+    campaigns = Campaign.all
+    render json: campaigns.to_json
+  end
+
+  def create
+    campaign = Campaign.new(campaign_params)
+    if campaign.save
+      render json: campaign.to_json, status: :created
+    else
+      render json: campaign.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    campaign = Campaign.find(params[:id])
+    render json: campaign, status: :ok
+  end
+
+  private
+
+  def campaign_params
+    params.require(:campaign).permit(:name, :description, :start_at, :finish_at)
+  end
+
+  def record_not_found(message)
+    render json: message.to_json, status: :unprocessable_entity
+  end
+end
---
+++ spec/controllers/campaigns_controller_spec.rb
@@ -0,0 +1,104 @@
+require 'rails_helper'
+
+RSpec.describe CampaignsController, type: :controller do
+  describe '#index' do
+    let(:campaigns) { Campaign.all }
+    let(:response_body) { JSON.parse(response.body) }
+
+    before do
+      get :index
+    end
+
+    it 'returns the expected account of campaigns' do
+      expect(response_body.count).to eq campaigns.count
+    end
+  end
+
+  describe '#create' do
+    context 'when the parameters are valid' do
+      let(:response_body) { JSON.parse(response.body) }
+
+      let(:params) do
+        {
+          campaign: {
+            name: 'New campaign',
+            description: 'Some here',
+            start_at: '15/12/2022',
+            finish_at: '18/12/2022'
+          }
+        }
+      end
+
+      before do
+        post :create, params: params
+      end
+
+      it { expect(response).to have_http_status(201) }
+
+      it 'creates the new campaign' do
+        expect(response_body["name"]).to eq params[:campaign][:name]
+        expect(response_body["description"]).to eq params[:campaign][:description]
+        expect(response_body["start_at"].to_datetime.strftime('%d/%m/%Y')).to eq params[:campaign][:start_at]
+        expect(response_body["finish_at"].to_datetime.strftime('%d/%m/%Y')).to eq params[:campaign][:finish_at]
+      end
+    end
+
+    context "when the missing parameters" do
+      let(:response_body) { JSON.parse(response.body) }
+
+      let(:params) do
+        {
+          campaign: {
+            description: 'Some here',
+            start_at: '15/12/2022',
+            finish_at: '18/12/2022'
+          }
+        }
+      end
+
+      before do
+        post :create, params: params
+      end
+
+      it { expect(response).to have_http_status(422) }
+
+      it 'returns message error' do
+        expect(response_body).to eq({"name"=>["can't be blank"]})
+      end
+    end
+  end
+
+  describe '#show' do
+    context 'when the id exist' do
+      let(:campaign) { campaigns(:campaign1) }
+      let(:response_body) { JSON.parse(response.body) }
+
+      before do
+        get :show, params: { id: campaign.id }
+      end
+
+      it { expect(response).to have_http_status(200) }
+
+      it 'returns the campaign information' do
+        expect(response_body["name"]).to eq campaign.name
+        expect(response_body["description"]).to eq campaign.description
+        expect(response_body["start_at"].to_datetime.strftime('%d/%m/%Y')).to eq campaign.start_at.strftime('%d/%m/%Y')
+        expect(response_body["finish_at"].to_datetime.strftime('%d/%m/%Y')).to eq campaign.finish_at.strftime('%d/%m/%Y')
+      end
+    end
+
+    context "when the id doesn't exist" do
+      let(:response_body) { JSON.parse(response.body) }
+
+      before do
+        get :show, params: { id: 123 }
+      end
+
+      it { expect(response).to have_http_status(422) }
+
+      it 'returns message error' do
+        expect(response_body).to eq("Couldn't find Campaign with 'id'=123")
+      end
+    end
+  end
+end
