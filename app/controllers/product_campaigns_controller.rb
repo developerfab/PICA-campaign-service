b4c7d8b2333804f0d6f2e456a939b620eca82890
developerfab <fab7696550@gmail.com>
2020-11-14 17:32:34 -0500

    Add create product
    It includes create the products for a campaign.

--- app/controllers/product_campaigns_controller.rb
+++ app/controllers/product_campaigns_controller.rb
@@ -5,4 +5,19 @@ class ProductCampaignsController < ApplicationController
     product_campaigns = ProductCampaign.filter_by_campaign_id(params[:campaign_id])
     render json: product_campaigns.to_json
   end
+
+  def create
+    product = ProductCampaign.new(product_campaign_params)
+    if product.save
+      render json: product.to_json, status: :created
+    else
+      render json: { errors: product.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_campaign_params
+    params.require(:products_campaign).permit(:campaign_id, :product_id, :value, :discount)
+  end
 end
--- spec/controllers/product_campaigns_controller_spec.rb
+++ spec/controllers/product_campaigns_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe ProductCampaignsController, type: :controller do
+RSpec.describe ProductCampaignsController, type: :request do
   describe '#index' do
     let(:campaign) { campaigns(:campaign1) }
     let(:response_body) { JSON.parse(response.body) }
 
     before do
-      get :index, params: { campaign_id: campaign.id }
+      get campaign_product_campaigns_path(campaign_id: campaign.id)
     end
 
     it { expect(response_body.count).to eq(2) }
@@ -14,6 +14,62 @@ RSpec.describe ProductCampaignsController, type: :controller do
     it 'returns the products of the campaign' do
       response_body.each do |product|
         expect(product['campaign_id']).to eq(campaign.id)
+      end
+    end
+  end
+
+  describe '#create' do
+    context 'when the parameters are correct' do
+      let(:campaign) { campaigns(:campaign1) }
+      let(:response_body) { JSON.parse(response.body) }
+
+      let(:params) do
+        {
+          products_campaign: {
+            campaign_id: campaign.id,
+            product_id: 3,
+            value: 60_000,
+            discount: 0.4
+          }
+        }
+      end
+
+      before do
+        post campaign_product_campaigns_path(campaign_id: campaign.id), params: params
+      end
+
+      it { expect(response).to have_http_status(201) }
+
+      it 'returns the product information' do
+        expect(response_body).to include('campaign_id')
+        expect(response_body).to include('product_id')
+        expect(response_body).to include('value')
+        expect(response_body).to include('discount')
+      end
+    end
+
+    context 'when the missing parameters' do
+      let(:campaign) { campaigns(:campaign1) }
+      let(:response_body) { JSON.parse(response.body) }
+
+      let(:params) do
+        {
+          products_campaign: {
+            product_id: 3,
+            value: 60_000,
+            discount: 0.4
+          }
+        }
+      end
+
+      before do
+        post campaign_product_campaigns_path(campaign_id: campaign.id), params: params
+      end
+
+      it { expect(response).to have_http_status(422) }
+
+      it 'returns a message error' do
+        expect(response_body['errors']).to eq({"campaign"=>["must exist"]})
       end
     end
   end
